c270519ae25f45a75507fc49a00b1cf0bf08beee
Garren Staubli <gstaubli@demandbase.com>
2013-07-14 15:46:57 -0700

    Cleaned up tests for brevity and stricter requirements

--- spec/increments_spec.rb
+++ spec/increments_spec.rb
@@ -2,41 +2,29 @@ require 'spec_helper'
 
 describe Increments do
   before(:each) do
-    @options = { 
-                  :min => 1, 
-                  :max => 5000, 
-                  :increment => 1000 
-               }
+    @options = { :min       => 1, 
+                 :max       => 5000, 
+                 :increment => 1000 }
   end
 
-  describe "increment" do
+  describe "#increment" do
     describe "success" do
       it "should build valid incrementing batches" do
-        Increments.increment(@options) do |min,max|
-          (min <= @options[:max]).should be_true
-          (max <= @options[:max]).should be_true
-          (min <= max).should be_true
-          (max-min <= @options[:increment]).should be_true
-        end
+        batches = []
+        Increments.increment(@options) { |min,max| batches << [min,max] }
+        batches.should == [[1, 1000], [1001, 2000], [2001, 3000], [3001, 4000], [4001, 5000]]
       end
+
       it "should build a valid incrementing batch with same min and max" do
         @options[:min] = @options[:max]
-        Increments.increment(@options) do |min,max|
-          (min <= @options[:max]).should be_true
-          (max <= @options[:max]).should be_true
-          (min <= max).should be_true
-          (max-min <= @options[:increment]).should be_true
-        end
+        batches = []
+        Increments.increment(@options) { |min,max| batches << [min,max] }
+        batches.should == [[5000,5000]]
       end
+
       it "should not have overlapping range values" do
         range_values = []
-        Increments.increment(@options) do |min,max|
-          range_values << min << max
-          (min <= @options[:max]).should be_true
-          (max <= @options[:max]).should be_true
-          (min <= max).should be_true
-          (max-min <= @options[:increment]).should be_true
-        end
+        Increments.increment(@options) { |min,max| range_values << min << max }
         range_values.uniq.size.should == range_values.size
       end
     end
@@ -48,9 +36,8 @@ describe Increments do
         end.should raise_error(LocalJumpError,"no block given (yield)")
       end
 
-      it "should throw an error when passed without an invalid min and max" do
-        @options[:min] = 10000
-        @options[:max] = 9999
+      it "should throw an error when passed with an invalid min and max" do
+        @options[:min] = @options[:max] + 1
         lambda do
           Increments.increment(@options) { |min,max| }
         end.should raise_error(ArgumentError,/^Invalid Options/)
@@ -58,34 +45,24 @@ describe Increments do
     end
   end
 
-  describe "decrement" do
+  describe "#decrement" do
     describe "success" do
       it "should build valid decrementing batches" do
-        Increments.decrement(@options) do |min,max|
-          (min <= @options[:max]).should be_true
-          (max <= @options[:max]).should be_true
-          (min <= max).should be_true
-          (max-min <= @options[:increment]).should be_true
-        end
+        batches = []
+        Increments.decrement(@options) { |min,max| batches << [min,max] }
+        batches.should == [[4001, 5000], [3001, 4000], [2001, 3000], [1001, 2000], [1, 1000]]
       end
+
       it "should build a valid decrementing batch with same min and max" do
         @options[:min] = @options[:max]
-        Increments.decrement(@options) do |min,max|
-          (min <= @options[:max]).should be_true
-          (max <= @options[:max]).should be_true
-          (min <= max).should be_true
-          (max-min <= @options[:increment]).should be_true
-        end
+        batches = []
+        Increments.decrement(@options) { |min,max| batches << [min,max] }
+        batches.should == [[5000,5000]]
       end
+
       it "should not have overlapping range values" do
         range_values = []
-        Increments.decrement(@options) do |min,max|
-          range_values << min << max
-          (min <= @options[:max]).should be_true
-          (max <= @options[:max]).should be_true
-          (min <= max).should be_true
-          (max-min <= @options[:increment]).should be_true
-        end
+        Increments.decrement(@options) { |min,max| range_values << min << max }
         range_values.uniq.size.should == range_values.size
       end
     end
@@ -96,9 +73,9 @@ describe Increments do
           Increments.decrement(@options)
         end.should raise_error(LocalJumpError,"no block given (yield)")
       end
+
       it "should throw an error when passed without an invalid min and max" do
-        @options[:min] = 10000
-        @options[:max] = 9999
+        @options[:min] = @options[:max] + 1
         lambda do
           Increments.decrement(@options) { |min,max| }
         end.should raise_error(ArgumentError,/^Invalid Options/)
